adb525c4a9cbe0714820f861908134dbef4cb378
StefaneLohanna <stefane.lohanna.medeiros.guedes@ccc.ufcg.edu.br>
2025-02-04 21:41:06 -0300

    modificando funcionalidades básicas do tabuleiro e das jogadas

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,19 @@
 module Main (main) where
-import Utils.Matriz
+import Utils.Jogo
+import Utils.Tabuleiro
+
+-- main :: IO ()
+-- main = do
+--     -- Criação de três matrizes de mesma dimensão para representar cada tabuleiro temporal
+--     let matrixPassado = matrizString 4 4 "▢" -- Caso o ▢ não funcione, trocar por []
+--     let matrizPresente = matrizString 4 4 "▢"
+--     let matrizFuturo = matrizString 4 4 "▢"
+--     startTabuleiro [matrixPassado, matrizPresente, matrizFuturo]
 
 main :: IO ()
 main = do
-    -- Criação de três matrizes de mesma dimensão para representar cada tabuleiro temporal
-    let matrixPassado = matrizString 4 4 "▢" -- Caso o ▢ não funcione, trocar por []
-    let matrizPresente = matrizString 4 4 "▢"
-    let matrizFuturo = matrizString 4 4 "▢"
-    startTabuleiro [matrixPassado, matrizPresente, matrizFuturo]
+    let tabuleiroPassado = tabuleiro4x4
+    let tabuleiroPresente = tabuleiro4x4
+    let tabuleiroFuturo = tabuleiro4x4
+
+    jogar tabuleiroPassado tabuleiroPresente tabuleiroFuturo jogador1
--- src/Utils/Jogo.hs
+++ src/Utils/Jogo.hs
@@ -1,46 +1,4 @@
-import Data.List (intercalate)
-
--- -- Criando o tabuleiro 
--- type Tabuleiro = [[String]]
-
--- tabuleiro4x4 :: Tabuleiro
--- tabuleiro4x4 = replicate 4 (replicate 4 "\x25A1") -- repetindo o símbolo do quadrado usado
-
--- -- Definindo os emojis dos jogadores
--- jogador1 :: String
--- jogador1 = "\x265F"
-
--- jogador2 :: String
--- jogador2 = "\x2659"
-
--- -- Função para formatar o tabuleiro como uma String 
--- formatarTabuleiro :: Tabuleiro -> [String]
--- formatarTabuleiro tabuleiro = 
---     let tamanho = length tabuleiro
---     -- definindo as bordas
---         bordaSuperior = replicate (tamanho * 2 + 1) '_'  
---         bordaInferior = replicate (tamanho * 2 + 1) '¯' 
---         bordaLateral = map (\linha -> "|" ++ unwords linha ++ "|") tabuleiro
---     in bordaSuperior : bordaLateral ++ [bordaInferior]-- juntando as bordas
-
--- -- Função para imprimir os 3 tabuleiros
--- imprimirTabuleiros :: Tabuleiro -> Tabuleiro -> Tabuleiro -> IO ()
--- imprimirTabuleiros t1 t2 t3 = do
---     -- formatando os 3 tabuleiros para a impressão
---     let tabuleiro1 = formatarTabuleiro t1
---         tabuleiro2 = formatarTabuleiro t2
---         tabuleiro3 = formatarTabuleiro t3
-
---     --organizando os tabuleiros para a impressao
---     mapM_ (\(linha1, linha2, linha3) -> putStrLn $ linha1 ++ "   " ++ linha2 ++ "   " ++ linha3)
---           (zip3 tabuleiro1 tabuleiro2 tabuleiro3)
-
--- -- Atualiza o tabuleiro com as jogadas
--- atualizarTabuleiro :: Tabuleiro -> Int -> Int -> String -> Tabuleiro
--- atualizarTabuleiro tabuleiro linha coluna jogador = 
---     take linha tabuleiro ++ 
---     [take coluna (tabuleiro !! linha) ++ [jogador] ++ drop (coluna +  1) (tabuleiro !! linha)] ++ drop (linha + 1) tabuleiro
-
+module Utils.Jogo (jogar) where
 
 -- Solicitar jogada do jogador
 obterJogada :: IO (Int, Int)
@@ -103,14 +61,3 @@ definirFoco = do
     else do
         putStrLn "Opção Inválida"
         definirFoco  -- Chama a função novamente até que o jogador insira um valor válido.
-
--- Main para permitir a execução do programa
-main :: IO ()
-main = do
-    let tabuleiroPassado = tabuleiro4x4
-    let tabuleiroPresente = tabuleiro4x4
-    let tabuleiroFuturo = tabuleiro4x4
-
-    jogar tabuleiroPassado tabuleiroPresente tabuleiroFuturo jogador1
-
-
