b679a061c98d3a04e62062262fc1c41aab40fa35
anapaulab3rto <anapaulab3rto@gmail.com>
2025-02-08 21:11:04 -0300

    add arvores, selecao acao do jogador, alteracao dos icones

--- src/Utils/Jogo.hs
+++ src/Utils/Jogo.hs
@@ -1,56 +1,18 @@
 module Utils.Jogo (iniciarTabuleiro) where
- 
 import System.IO (hFlush, stdout)
 import Text.Read (readMaybe)
 import Utils.Tabuleiro
 
 iniciarTabuleiro :: IO ()
 iniciarTabuleiro = do
-    let tabuleiroPassado = inicializarTabuleiro
-    let tabuleiroPresente = inicializarTabuleiro
-    let tabuleiroFuturo = inicializarTabuleiro
+    let tabuleiro1 = inicializarTabuleiro tabuleiro4x4 0 0 jogador1
+    let tabuleiro = inicializarTabuleiro tabuleiro1 3 3 jogador2
+
+    let tabuleiroPassado = tabuleiro
+    let tabuleiroPresente = tabuleiro
+    let tabuleiroFuturo = tabuleiro
+
     jogar tabuleiroPassado tabuleiroPresente tabuleiroFuturo jogador1
-
-
-
-{- Loop do jogo, alternando entre os jogadores
-jogar :: Tabuleiro -> Tabuleiro -> Tabuleiro -> String -> IO ()
-jogar tPassado tPresente tFuturo jogadorAtual = do
-    putStrLn "\nTabuleiros atuais:"
-    imprimirTabuleiros tPassado tPresente tFuturo
-
-    putStrLn $ "\nVez do jogador: " ++ jogadorAtual
-    foco <- definirFoco
-    (linha, coluna) <- obterJogada
-
-    --usar guardas
-    let tabuleiroSelecionado = case foco of
-            "passado" -> tPassado
-            "presente" -> tPresente
-            "futuro" -> tFuturo
-            _ -> tPresente -- fallback para evitar erros
-
-    let posicaoAtual = acharJogador jogadorAtual tabuleiroSelecionado
-
-    case posicaoAtual of
-        Just (linhaAntiga, colunaAntiga) ->
-            if movimentoValido (linhaAntiga, colunaAntiga) (linha, coluna) then do
-                let novoTabuleiro = atualizarTabuleiro tabuleiroSelecionado linhaAntiga colunaAntiga linha coluna jogadorAtual
-                let (novoTPassado, novoTPresente, novoTFuturo) = case foco of
-                        "passado"  -> (novoTabuleiro, tPresente, tFuturo)
-                        "presente" -> (tPassado, novoTabuleiro, tFuturo)
-                        "futuro"   -> (tPassado, tPresente, novoTabuleiro)
-                        _          -> (tPassado, tPresente, tFuturo)
-
-                let proximoJogador = if jogadorAtual == jogador1 then jogador2 else jogador1
-                jogar novoTPassado novoTPresente novoTFuturo proximoJogador
-            else do
-                putStrLn "Movimento inválido! Você só pode se mover uma casa na horizontal ou na vertical."
-                jogar tPassado tPresente tFuturo jogadorAtual
-        Nothing -> do
-            putStrLn "Erro: Jogador não encontrado!"
-            jogar tPassado tPresente tFuturo jogadorAtual
--}
 
 -- Loop do jogo, alternando entre os jogadores
 jogar :: Tabuleiro -> Tabuleiro -> Tabuleiro -> String -> IO ()
@@ -60,7 +22,7 @@ jogar tPassado tPresente tFuturo jogadorAtual = do
 
     putStrLn $ "\nVez do jogador: " ++ jogadorAtual
     foco <- definirFoco
-    (linha, coluna) <- obterJogada
+    (linha, coluna, jogada) <- escolherJogada
 
     -- usar guardas
     let tabuleiroSelecionado
@@ -70,6 +32,52 @@ jogar tPassado tPresente tFuturo jogadorAtual = do
           | otherwise          = tPresente -- fallback para evitar erros
 
     let posicaoAtual = acharJogador jogadorAtual tabuleiroSelecionado
+
+    case posicaoAtual of
+        Just (linhaAntiga, colunaAntiga) ->
+            if movimentoValido (linhaAntiga, colunaAntiga) (linha, coluna) then do
+
+                if (jogada == "a") then do 
+                    let proximoJogador = if jogadorAtual == jogador1 then jogador2 else jogador1
+                    if(foco == "passado") then do 
+                        let novoTabuleiro = plantarSementeNoTabuleiro tabuleiroSelecionado linha coluna semente
+                        let novoTabuleiroPresente = plantarSementeNoTabuleiro tPresente linha coluna arbusto
+                        let novoTabuleiroFuturo = plantarSementeNoTabuleiro tFuturo linha coluna arvore
+                        
+                        jogar novoTabuleiro novoTabuleiroPresente novoTabuleiroFuturo proximoJogador
+
+                    else if(foco == "presente") then do 
+                        let novoTabuleiro = plantarSementeNoTabuleiro tabuleiroSelecionado linha coluna semente
+                        let novoTabuleiroFuturo = plantarSementeNoTabuleiro tFuturo linha coluna arbusto
+
+                        jogar tPassado novoTabuleiro novoTabuleiroFuturo proximoJogador
+
+                    else if(foco == "futuro") then do 
+                        let novoTabuleiro = plantarSementeNoTabuleiro tabuleiroSelecionado linha coluna semente
+
+                        jogar tPassado tPresente novoTabuleiro proximoJogador
+
+                    else jogar tPassado tPresente tFuturo proximoJogador
+                    
+                    
+                else do 
+                    let novoTabuleiro = atualizarTabuleiro tabuleiroSelecionado linhaAntiga colunaAntiga linha coluna jogadorAtual
+                    
+                    let (novoTPassado, novoTPresente, novoTFuturo)
+                          | foco == "passado" = (novoTabuleiro, tPresente, tFuturo)
+                          | foco == "presente" = (tPassado, novoTabuleiro, tFuturo)
+                          | foco == "futuro" = (tPassado, tPresente, novoTabuleiro)
+                          | otherwise        = (tPassado, tPresente, tFuturo)
+
+                    let proximoJogador = if jogadorAtual == jogador1 then jogador2 else jogador1
+                    jogar novoTPassado novoTPresente novoTFuturo proximoJogador
+
+            else do
+                putStrLn "Movimento inválido! Você só pode se mover uma casa na horizontal ou na vertical."
+                jogar tPassado tPresente tFuturo jogadorAtual
+        Nothing -> do
+            putStrLn "Erro: Jogador não encontrado!"
+            jogar tPassado tPresente tFuturo jogadorAtual
 
     case posicaoAtual of
         Just (linhaAntiga, colunaAntiga) ->
@@ -92,13 +100,37 @@ jogar tPassado tPresente tFuturo jogadorAtual = do
             jogar tPassado tPresente tFuturo jogadorAtual
 
 
+
+--Função onde o jogador escolhe tipo de jogada
+escolherJogada :: IO (Int, Int, String)
+escolherJogada = do 
+    putStr "Escolha qual será sua próxima rodada: (Movimentação (m), Plantar Árvore (a)) "
+    hFlush stdout
+    escolha <- getLine
+    if escolha == "m" then do
+        (linha, coluna) <- obterJogada
+        return (linha, coluna, escolha)
+    else if escolha == "a" then do
+        (linha, coluna) <- obterLocalSemente
+        return (linha, coluna, escolha)
+    else do 
+        putStrLn "Entrada inválida!"
+        escolherJogada
+
 -- Solicitar jogada do jogador
 obterJogada :: IO (Int, Int)
 obterJogada = do
     linha <- obterPosicao "Digite a linha (1 a 4): "
     coluna <- obterPosicao "Digite a coluna (1 a 4): "
     return (linha, coluna)
-   
+
+-- Função para plantar semente
+obterLocalSemente :: IO (Int, Int)
+obterLocalSemente = do
+    linha <- obterPosicao "Digite a linha onde deseja plantar a semente: (1 a 4)"
+    coluna <- obterPosicao "Digite a coluna onde deseja plantar a semente: (1 a 4)"
+    return (linha, coluna)
+
 -- Função para obter um número válido dentro de um intervalo específico
 obterPosicao :: String -> IO Int
 obterPosicao mensagem = do
@@ -132,3 +164,4 @@ definirFoco = do
     else do
         putStrLn "Opção Inválida"
         definirFoco  -- Chama a função novamente até que o jogador insira um valor válido.
+
--- src/Utils/Tabuleiro.hs
+++ src/Utils/Tabuleiro.hs
@@ -1,35 +1,26 @@
-module Utils.Tabuleiro  (Tabuleiro, imprimirTabuleiros, atualizarTabuleiro, jogador1, jogador2, tabuleiro4x4, inicializarTabuleiro, movimentoValido,acharJogador) where
+module Utils.Tabuleiro  (Tabuleiro, imprimirTabuleiros, atualizarTabuleiro, jogador1, jogador2, tabuleiro4x4, inicializarTabuleiro, movimentoValido,acharJogador, semente, arbusto, arvore, plantarSementeNoTabuleiro) where
 
 -- Criando o tabuleiro 
 type Tabuleiro = [[String]]
 
 tabuleiro4x4 :: Tabuleiro
-tabuleiro4x4 = replicate 4 (replicate 4 "\x25A1") -- repetindo o símbolo do quadrado vazio ☐
+tabuleiro4x4 = replicate 4 (replicate 4 "\x1F533") -- repetindo o símbolo do quadrado vazio ☐
 
 -- Definindo os emojis dos jogadores
 jogador1 :: String
-jogador1 = "\x265F"  
+jogador1 = "\x1F98A"  
 
 jogador2 :: String
-jogador2 = "\x2659"  
+jogador2 = "\x1F407"
 
--- Inicializa o tabuleiro com os jogadores nas posições iniciais
-inicializarTabuleiro :: Tabuleiro
-inicializarTabuleiro = 
-    [ [jogador1, "☐", "☐", "☐"]
-    , ["☐", "☐", "☐", "☐"]
-    , ["☐", "☐", "☐", "☐"]
-    , ["☐", "☐", "☐", jogador2]
-    ]
+semente :: String 
+semente = "\x1F330"
 
-{--inicializarTabuleiro :: String -> String -> Tabuleiro
-inicializarTabuleiro jogador1 jogador2 =
-    [ [posicao (r, c) | c <- [0..3]] | r <- [0..3] ]
-  where
-    posicao (0,0) = jogador1  -- jogador 1
-    posicao (3,3) = jogador2  -- jogador 2
-    posicao _     = ???       -- Casas vazias
---}
+arbusto :: String
+arbusto = "\x1F331"
+
+arvore :: String
+arvore = "\x1F333"
 
 
 -- Função para formatar o tabuleiro como uma String 
@@ -49,29 +40,26 @@ imprimirTabuleiros t1 t2 t3 = do
     let tabuleiro1 = formatarTabuleiro t1
         tabuleiro2 = formatarTabuleiro t2
         tabuleiro3 = formatarTabuleiro t3
-
     --organizando os tabuleiros para a impressao
     mapM_ (\(linha1, linha2, linha3) -> putStrLn $ linha1 ++ "   " ++ linha2 ++ "   " ++ linha3)
-          (zip3 tabuleiro1 tabuleiro2 tabuleiro3)
+        (zip3 tabuleiro1 tabuleiro2 tabuleiro3)
 
 -- Atualiza o tabuleiro com as jogadas, removendo a peça da posição antiga
 atualizarTabuleiro :: Tabuleiro -> Int -> Int -> Int -> Int -> String -> Tabuleiro
 atualizarTabuleiro tabuleiro linhaAntiga colunaAntiga linhaNova colunaNova jogador =
-    let tabuleiroSemJogador = modificarTabuleiro tabuleiro linhaAntiga colunaAntiga "☐"  -- Remove a peça antiga
+    let tabuleiroSemJogador = modificarTabuleiro tabuleiro linhaAntiga colunaAntiga "\x1F533"  -- Remove a peça antiga
     in modificarTabuleiro tabuleiroSemJogador linhaNova colunaNova jogador  -- Adiciona a peça na nova posição
 
 -- Atualiza o tabuleiro com as jogadas
 modificarTabuleiro :: Tabuleiro -> Int -> Int -> String -> Tabuleiro
 modificarTabuleiro tabuleiro linha coluna jogador = 
     take linha tabuleiro ++ [take coluna (tabuleiro !! linha) ++ [jogador] ++ drop (coluna +  1) (tabuleiro !! linha)] ++ drop (linha + 1) tabuleiro
-
+    
 -- Atualiza o tabuleiro com as jogadas
-{--atualizarTabuleiro :: Tabuleiro -> Int -> Int -> String -> Tabuleiro
-atualizarTabuleiro tabuleiro linha coluna jogador = 
-    take linha tabuleiro ++ 
-    [take coluna (tabuleiro !! linha) ++ [jogador] ++ drop (coluna +  1) (tabuleiro !! linha)] ++ drop (linha + 1) tabuleiro
---}
-
+inicializarTabuleiro :: Tabuleiro -> Int -> Int -> String -> Tabuleiro
+inicializarTabuleiro tabuleiro linha coluna jogador = 
+    take linha tabuleiro ++ [take coluna (tabuleiro !! linha) ++ [jogador] ++ drop (coluna +  1) (tabuleiro !! linha)] ++ drop (linha + 1) tabuleiro
+    
 -- Colocar função de foco aqui )para a representação gráfica no jogo)
 
 -- Verifica se o movimento é válido (não diagonal e apenas uma casa por vez)
@@ -86,8 +74,11 @@ movimentoValido (linhaAntiga, colunaAntiga) (linhaNova, colunaNova) = -- posicao
 acharJogador :: String -> Tabuleiro -> Maybe (Int, Int) 
 acharJogador jogador tabuleiro = 
     let posicoes = [(linha, coluna) | (linha, linhaVals) <- zip [0,1,2,3] tabuleiro, -- Associa cada linha do tabuleiro ao seu índice (0, 1, 2, 3).
-                                      (coluna, valor) <- zip [0,1,2,3] linhaVals, -- Associa cada coluna ao seu índice dentro da linha. 
-                                      valor == jogador]-- Filtra apenas as posições onde o símbolo do jogador está.
+                                    (coluna, valor) <- zip [0,1,2,3] linhaVals, -- Associa cada coluna ao seu índice dentro da linha. 
+                                    valor == jogador]-- Filtra apenas as posições onde o símbolo do jogador está.
     in if null posicoes then Nothing else Just (head posicoes)
     --null posicoes verifica se a lista de posições está vazia:
     -- se tiver vazia: retorna Nothing (o jogador não foi encontrado) se n tiver: retorna Just (head posicoes), pegando a primeira posição encontrada.
+plantarSementeNoTabuleiro :: Tabuleiro -> Int -> Int -> String -> Tabuleiro
+plantarSementeNoTabuleiro tabuleiro linha coluna planta=
+    take linha tabuleiro ++ [take coluna (tabuleiro !! linha) ++ [planta] ++ drop (coluna + 1) (tabuleiro !! linha)] ++ drop (linha + 1) tabuleiro
